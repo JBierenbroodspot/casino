36c89a3c4443cf728ddcae02f0131af91e44aed9
165425 <165425@novacollege.nl>
2021-01-22 16:51:16 +0100

    Add test_cards module to tests

---
+++ casino/tests/test_cards.py
@@ -0,0 +1,11 @@
+from casino.games import cards
+
+deck = cards.DeckOfCards(jokers=True)
+deck.shuffle_cards()
+print("random card w/o discard:", deck.pick_random_card().show())
+print("random card w/ discard:", deck.pick_top_card(True).show())
+print("deck length:", len(deck.cards))
+print("discarded cards:", deck.discarded_cards)
+print("shuffle including random cards:", deck.shuffle_cards(True))
+print("deck length post shuffle:", len(deck.cards))
+print("discarded cards post shuffle:", deck.discarded_cards)
